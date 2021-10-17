51662be1a1dfc8b15ea3dd8767811036b017370c
Ivan Alejandro Estrella Arciniega <huezos_0708@hotmail.com>
2021-10-16 22:35:54 -0500

    Filter size opportunities

--- public/js/index.js
+++ public/js/index.js
@@ -4,18 +4,34 @@ new Vue({
         opportunities: [],
         total: 0,
         size: 10,
+        loading: true,
+        sizes: [10, 20, 50, 100],
+        textSearch: "",
     },
     mounted: function(){
-        var elementVue = this;
-        axios.post("https://search.torre.co/opportunities/_search/", {
-                size: elementVue.size
-            })
-            .then(function(response){
-                elementVue.total = response.total;
-                elementVue.opportunities = response.results;
-            });
+        this.searchOpportunities();
     },
     methods: {
-
+        /**
+         * Function to search opportunities
+         */
+        searchOpportunities: function(){
+            var elementVue = this;
+            this.loading = true;
+            axios.post("https://search.torre.co/opportunities/_search/?" +
+                "size=" + this.size)
+                .then(function(response){
+                    elementVue.saveOpportunities(response.data);
+                });
+        },
+        /**
+         * Functio to save the opportunities
+         * @param object data
+         */
+        saveOpportunities: function(data){
+            this.total = data.total;
+            this.opportunities = data.results;
+            this.loading = false;
+        },
     },
 });
